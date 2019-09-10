b4366136590e6964a236e19ac875a5f609d5916a
Phu T. Van <pvan@fredhutch.org>
2019-09-10 12:57:05 -0700

    added #2

---
+++ euler002.R
@@ -0,0 +1,18 @@
+# Project Euler problem 002:
+# By considering the terms in the Fibonacci sequence whose values do not exceed four million, 
+# find the sum of the even-valued terms.
+
+fib <- function(upper){
+  vec <- c(1,2)
+  while (vec[length(vec)] < upper){
+    val <- vec[length(vec)] + vec[length(vec)-1]
+    if(val < upper){
+      vec <- c(vec, val)
+    } else {break}
+  }
+  return(vec)
+}
+
+vec <- fib(4e6)
+
+sum(vec[vec%%2==0])
