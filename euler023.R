f70a138e60c7ad994c0d355418c1545ec21d66ee
Phu T. Van <pvan@fredhutch.org>
2019-09-23 11:30:54 -0700

    added #23 non-abundant sums

--- euler023.R
+++ euler023.R
@@ -4,18 +4,17 @@
 
 # A number n is called deficient if the sum of its proper divisors is less than n 
 # and it is called abundant if this sum exceeds n.
-
 # As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, 
 # the smallest number that can be written as the sum of two abundant numbers is 24. 
-
 # By mathematical analysis, it can be shown that all integers greater than 28123 
 # can be written as the sum of two abundant numbers. 
-
 # However, this upper limit cannot be reduced any further by analysis 
 # even though it is known that the greatest number that cannot be expressed 
 # as the sum of two abundant numbers is less than this limit.
-
 # Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+
+# we likely could have implemented our own version of divisors() but likely
+# there won't be much performance gain...
 
 library(numbers)
 abundant <- c()
